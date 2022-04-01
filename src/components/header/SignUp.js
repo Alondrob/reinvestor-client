95e5bea5e4d55ef7dccf9f37495a43fc57128fd3
Alondrob <alondrob@gmail.com>
2022-03-31 23:47:31 -0400

    2

--- src/components/header/SignUp.js
+++ src/components/header/SignUp.js
@@ -1,33 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {useDispatch, useSelector}  from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import {register, reset} from '../../features/authSlice'
 
-function SignIn() {
+function SignUp() {
+    const [formData, setFormData] = useState({
+      name: '',
+      email: '',
+      password: '',
+      passwordConfirmation: ''
+    })
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    
+
+   const handleSubmit = () => {}
+
   return (
     <div class="w-full max-w-xs justify-center ">
       <div className="bg-pink-100 md:items-center">
-        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 ">
-          <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-              Username
+        <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 ">
+
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
+              User Name
             </label>
-            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 
+                               text-gray-700 leading-tight focus:outline-none 
+                               focus:shadow-outline"  type="text" placeholder="Username" 
+                               onChange={(e) => setFormData({...formData, name:e.target.value})}
+                               value={formData.name}
+                               />
           </div>
-          <div class="mb-6">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
+              Email
+            </label>
+            <input className="shadow appearance-none border rounded w-full py-2 px-3
+                             text-gray-700 leading-tight focus:outline-none 
+                             focus:shadow-outline"  type='email' placeholder="Username" 
+                             onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                             value={formData.email}
+                             />
+          </div>
+
+          <div className="mb-6">
+            <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
               Password
             </label>
-            <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
-            <p class="text-red-500 text-xs italic">Please choose a password.</p>
+            <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 
+                              text-gray-700 mb-3 leading-tight focus:outline-none 
+                               focus:shadow-outline" type="password" placeholder="******************" 
+                               onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+                               value={formData.password}
+                               />
+                              <p className="text-red-500 text-xs italic">Please choose a password.</p>
           </div>
-          <div class="flex items-center justify-between">
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+
+          <div className="mb-6">
+            <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
+              Confirm Password
+            </label>
+            <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 
+                              text-gray-700 mb-3 leading-tight focus:outline-none 
+                               focus:shadow-outline" type="password" placeholder="******************" 
+                               onChange={(e) => setFormData({ ...formData, passwordConfirmation: e.target.value })}
+                               value={formData.passwordConfirmation}
+                               />
+                              <p className="text-red-500 text-xs italic">Please choose a password.</p>
+          </div>
+
+          
+          
+          <div className="flex items-center justify-between">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
               Sign In
             </button>
-            <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+            <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
               Forgot Password?
             </a>
           </div>
         </form>
-        <p class="text-center text-gray-500 text-xs">
+        <p className="text-center text-gray-500 text-xs">
           &copy;2020 Acme Corp. All rights reserved.
         </p>
       </div>
@@ -36,4 +93,4 @@ function SignIn() {
   )
 }
 
-export default SignIn
+export default SignUp
